cd8e15a137c7749c398d77e6e6fe2ee85cf5d880
Caleb Bareman <cbareman99@gmail.com>
2024-12-18 20:55:04 -0800

    Day 17

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -20,16 +20,6 @@ impl<'p> Computer<'p> {
         self.output
     }
 
-    pub fn produces_copy(mut self) -> bool {
-        while self.pointer < self.program.len() {
-            self.step();
-            if !self.program.starts_with(&self.output) {
-                return false;
-            }
-        }
-        self.program == self.output
-    }
-
     fn step(&mut self) {
         let instruction = self.program[self.pointer];
         let arg = self.program[self.pointer + 1];
@@ -127,60 +117,6 @@ fn part_one(input: &str) -> String {
     })
 }
 
-fn reverse(program: &[u8]) -> Num {
-    let (mut a, mut b, mut c) = (0, 0, 0); // Not sure yet how to initialize B and C
-    let mut pointer = program.len();
-    let mut output = program;
-    while !output.is_empty() || pointer > 0 {
-        pointer = pointer.checked_sub(2).unwrap_or(program.len() - 4);
-        let instruction = program[pointer];
-        let arg = program[pointer + 1];
-        match instruction {
-            0 => a <<= combo_val(arg, (a, b, c)), // adv
-            1 => b ^= arg as Num,                 // bxl
-            // 2 => b = combo_val(arg, (a, b, c)) & 0b111, // todo
-            2 => {
-                let bv = b;
-                let Some(reg) = combo(arg, (&mut a, &mut b, &mut c)) else {
-                    continue;
-                };
-                *reg = (*reg & !0b111) | (bv & 0b111);
-            }
-            3 => (), // jnz -noop
-            4 => b ^= c,
-            5 => {
-                let out: &u8;
-                (out, output) = output.split_last().unwrap();
-                let reg = combo(arg, (&mut a, &mut b, &mut c)).unwrap();
-                *reg = (*reg & !0b111) | (*out as Num);
-            }
-            6 => b = a << combo_val(arg, (a, b, c)), // not sure what to do with these
-            7 => c = a << combo_val(arg, (a, b, c)),
-            _ => unreachable!(),
-        }
-    }
-    a
-}
-fn combo<'a>(arg: u8, (a, b, c): (&'a mut Num, &'a mut Num, &'a mut Num)) -> Option<&mut Num> {
-    match arg {
-        0..=3 => None,
-        4 => Some(a),
-        5 => Some(b),
-        6 => Some(c),
-        7 => panic!("Reserved"),
-        _ => unreachable!(),
-    }
-}
-fn combo_val(arg: u8, (a, b, c): (Num, Num, Num)) -> Num {
-    match arg {
-        a @ 0..=3 => a as Num,
-        4 => a,
-        5 => b,
-        6 => c,
-        7 => panic!("Reserved"),
-        _ => unreachable!(),
-    }
-}
 fn part_two(input: &str) -> Num {
     // likely need to solve rather than check all numbers Programs all seem
     // to end in "3,0" and have no other jumps, so we know A = 0 at the end.
@@ -219,44 +155,43 @@ fn part_two(input: &str) -> Num {
         .split(',')
         .map(|n| n.parse::<u8>().unwrap())
         .collect::<Vec<_>>();
-    let mut a = 0;
-    let mut computer = Computer {
+    let a = solve(0, (0, b, c), &program).unwrap();
+    let comp = Computer {
         program: &program,
         pointer: 0,
         a,
         b,
         c,
-        output: Vec::with_capacity(program.len()),
-    };
-    while computer.clone().run().len() < program.len() {
-        a += 10_000_000;
-        computer = Computer {
-            program: &program,
-            pointer: 0,
-            a,
-            b,
-            c,
-            output: Vec::with_capacity(program.len()),
+        output: Vec::new(),
+    }
+    .run();
+    assert_eq!(comp, program);
+    a
+}
+
+// build incrementally, 3 bits at a time
+// Help from: https://www.reddit.com/r/adventofcode/comments/1hg38ah/comment/m2pyn7q/
+fn solve(i: usize, (a, b, c): (Num, Num, Num), program: &[u8]) -> Option<Num> {
+    let output = Computer {
+        program,
+        pointer: 0,
+        a,
+        b,
+        c,
+        output: Vec::new(),
+    }
+    .run();
+    if output == program {
+        return Some(a);
+    }
+    if program[program.len() - i..] == output || i == 0 {
+        for n in 0..8usize {
+            if let Some(a) = solve(i + 1, ((a << 3) + n, b, c), program) {
+                return Some(a);
+            }
         }
     }
-    loop {
-        if a % 10_000_000 == 0 {
-            println!("{a}\r");
-        }
-        let computer = Computer {
-            program: &program,
-            pointer: 0,
-            a,
-            b,
-            c,
-            output: Vec::with_capacity(program.len()),
-        };
-        if computer.produces_copy() {
-            return a;
-        }
-        a += 1;
-    }
-    // reverse(&program)
+    None
 }
 
 #[cfg(test)]
